8b8e10862466f1f8792e809ebae999a3bcba56f9
Yash Jagdale <115516792+yash2690@users.noreply.github.com>
2025-08-27 12:55:36 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,64 @@
+# list
+#a=[1,2,3,4,"by",True]
+#a[0]=60
+#print(a)
+#o/p=[60,2,3,4,"by",True]
+
+
+# Append
+# a=[1,2,3,4,5]
+# a.append(6)
+# a.append("Patil")
+# print(a)
+#o/p = [1, 2, 3, 4, 5, 6, "Patil"]
+
+
+#Insert
+#a=[1,2,3,4,5]
+#a.insert(1,70)
+#print(a)
+#o/p = [1, 70, 2, 3, 4,5]
+
+#extend the list
+# a=[1,2,3,4,5]
+# a.extend([0,9,10])
+# print(a)
+#o/p = [1, 2, 3, 4, 5,0,9,10]
+
+
+#remove
+# a=[1,2,3,4,5]
+# a.remove(4)
+# print(a)
+#o/p = [1, 2, 3, 5]
+
+#pop
+# a=[1,2,3,4,5]
+# a.pop(2)
+# print(a)
+#o/p = [1, 2, 4, 5]
+
+#double
+# a=[1, 2, 3, 4, 5]
+# r=a*2
+# print(r)
+#o/p = [1, 2, 3, 4, 5,1,2,3,4,5]
+
+
+#average
+# g=[85,90,76,92,88]
+# average=sum(g)/len(g)
+# print("Average",average)
+#o/p Average 86.2
+
+#count
+# x=["Abesnt","Present","Abesnt","Abesnt","Present","Abesnt""Abesnt","Present","Abesnt"]
+# A=x.count("Abesnt")
+# print(A)
+#o/p 4
+
+
+#slicing
+# a = "Hello"
+# print(a[1:5]) 
+# o/t: ello
