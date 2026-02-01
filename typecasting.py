b1ccb11f8c6bbd4ff0a36fc3e85ae180a08af3db
Yash Jagdale <115516792+yash2690@users.noreply.github.com>
2026-02-01 18:52:10 +0530

    typecasting.py

--- typecasting.py
+++ typecasting.py
@@ -9,3 +9,20 @@ c= 1.9
 d=8
 print(c+d)
 
+# Python automatically converts 'a' to int 
+a = 7
+print(type(a)) 
+
+# Python automatically converts 'b' to float 
+b = 3.0
+print(type(b)) 
+
+# Python automatically converts 'c' to float as it is a float addition 
+c = a + b 
+print(c) 
+print(type(c))
+
+# Python automatically converts 'd' to float as it is a float multiplication
+d = a * b
+print(d)
+print(type(d))
