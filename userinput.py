693f473d87d84bc7993484ecf1c07b547236a5b1
Yash Jagdale <115516792+yash2690@users.noreply.github.com>
2026-02-02 20:15:37 +0530

    taking input form user

---
+++ userinput.py
@@ -0,0 +1,24 @@
+a = input("Enter your name : ")
+print("my name is ", a)
+
+#################
+
+x=int(input("enter your number:"))
+y=int(input("enter second number :"))
+
+print(x+y )
+
+#############
+
+num = input("Enter number:")
+print(num)
+name1 = input("Enter name: ")
+print(name1)
+
+print ("type of number", type(num))
+print ("type of name", type(name1))
+
+#############
+
+num = int(input("Enter a number: "))
+print(num, "is of type", type(num)) 
